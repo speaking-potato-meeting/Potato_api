6df3a1c421952a45b1b064855416777e73bfba46
an dong woo <akskwha@naver.com>
2023-09-19 17:43:56 +0900

    회원 crud

--- back/potato/api.py
+++ back/potato/api.py
@@ -5,11 +5,15 @@ from .models import Comment,User
 from datetime import date, datetime
 from ninja import Schema
 from django.shortcuts import get_object_or_404
-
-
+# from django.contrib.auth.models import User
+from django.http import Http404
+from datetime import date
+from typing import Optional
+from pydantic.networks import HttpUrl
 
 api  = NinjaAPI()
 
+#댓글
 class commentIn(Schema):
     user_id: int = None
     # user =1
@@ -17,7 +21,6 @@ class commentIn(Schema):
     # timestamp = datetime.now()
     text: str
     # text = "확인"
-
 
 class commentOut(Schema):
     id: int = None
@@ -29,14 +32,11 @@ class commentOut(Schema):
     text: str
     # text = "확인1"
 
-
-
 @api.post("/comments")
 def create_Comment(request, payload: commentIn):
     user = User.objects.get(id=payload.user_id)  # Fetch the User instance
     comment = Comment.objects.create(user=user, text=payload.text)
     return {"id": comment.id, "timestamp": comment.timestamp}
-
 
 @api.get('/comments/{comment_id}', response=commentOut)
 def get_Comment(request, comment_id: int):
@@ -57,3 +57,98 @@ def delete_Comment(request,comment_id: int):
     comment = get_object_or_404(Comment, id=comment_id)
     comment.delete()
     return {"success" : True}
+#댓글
+
+class CreateUserin(Schema):
+    username: str
+    password: str
+    email: str
+    phone: str
+    address: str
+    github: str
+    blog: Optional[HttpUrl] 
+    MBTI: str
+    postion: str
+    individual_rule: str
+    birth: date
+
+class CreateUserSchema(Schema):
+    username: str
+    password: str
+    email: str
+    phone: str
+    address: str
+    github: str
+    blog: Optional[HttpUrl] 
+    MBTI: str
+    postion: str
+    individual_rule: str
+    birth: date
+
+@api.post("/create-user")
+def create_user(request, data: CreateUserSchema):
+    user = User.objects.create_user(
+        username=data.username,
+        email=data.email,
+        password=data.password,
+        phone = data.phone,
+        address = data.address,
+        github = data.github,
+        blog = data.blog,
+        MBTI = data.MBTI,
+        postion = data.postion,
+        individual_rule = data.individual_rule,
+        birth = data.birth,
+    )
+    user.save()
+    return {"message": "성공"}
+
+@api.get("/get-user/{user_id}")
+def get_user(request, user_id: int):
+    try:
+        user = User.objects.get(id=user_id)
+
+        serialized_user = {
+            "username": user.username,
+            "email": user.email,
+            "phone": user.phone,
+            "address": user.address,
+            "github": user.github,
+            "blog": user.blog,
+            "MBTI": user.MBTI,
+            "postion": user.postion,
+            "individual_rule": user.individual_rule,
+            "birth": user.birth.strftime('%Y-%m-%d'),
+        }
+        return serialized_user
+    except User.DoesNotExist:
+        return {"message": "실패"}, 404
+    
+@api.put("/update-user/{user_id}")
+def update_user(request, user_id: int, data: CreateUserSchema):
+    try:
+        user = User.objects.get(id=user_id)
+        user.username = data.username
+        user.password=data.password
+        user.email = data.email
+        user.phone = data.phone
+        user.address = data.address
+        user.github = data.github
+        user.blog = data.blog
+        user.MBTI = data.MBTI
+        user.postion = data.postion
+        user.individual_rule = data.individual_rule
+        user.birth = data.birth
+        user.save()
+        return {"message": "성공"}
+    except User.DoesNotExist:
+        return {"message": "실패"}, 404
+    
+@api.delete("/delete-user/{user_id}")
+def delete_user(request, user_id: int):
+    try:
+        user = User.objects.get(id=user_id)
+        user.delete()
+        return {"message": "성공"}
+    except User.DoesNotExist:
+        return {"message": "실패"}, 404
