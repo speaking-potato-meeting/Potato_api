170df420fde96df75a7eed532a56fdec8c3b0c13
hong2HSH <tnghd1717@gmail.com>
2023-09-26 10:47:23 +0900

    스케줄,댓글 CRUD

--- back/potato/admin.py
+++ back/potato/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Comment,StudyTimer
+from .models import StudyTimer
 # Register your models here.
-admin.site.register(Comment)
 admin.site.register(StudyTimer)
--- back/schedule/api.py
+++ back/schedule/api.py
@@ -1,58 +1,64 @@
-from ninja import Schema, Router
-from potato.models import Comment, User
+from ninja import Schema, Router, Path, Query
+from potato.models import Comment, User, Schedule
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseServerError
 from datetime import date
+from pydantic import BaseModel
+from typing import List, Optional
 import logging
 
 router = Router()
 logger = logging.getLogger(__name__)
 
-class commentIn(Schema):
+class CommentIn(Schema):
+    schedule_id: int
     user_id: int
-    # user =1
     timestamp: date
-    # timestamp = datetime.now()
     text: str
-    # text = "확인"
 
 
-class commentOut(Schema):
+class CommentOut(Schema):
     id: int
-    # id=1
+    schedule_id: int
     user_id: int
-    # user =1
     timestamp: date
-    # timestamp = datetime.now()
     text: str
-    # text = "확인1"
 
+class ScheduleIn(Schema):
+    start_date: date
+    end_date: date
+    schedule: str
+    is_holiday: bool
 
+class ScheduleOut(ScheduleIn):
+    id: int
+
+#댓글작성
 @router.post("/comments")
-def create_Comment(request, payload: commentIn):
+def create_Comment(request, payload: CommentIn):
+    schedule_id = payload.schedule_id
     try:
-        user = User.objects.get(id=payload.user_id)  # Fetch the User instance
-        comment = Comment.objects.create(user=user, text=payload.text)
-        return {"id": comment.id, "timestamp": comment.timestamp}
-    except Exception as e:
-        #오류 로깅
-        logger.exception("An error occurred: %s", e)
-        return HttpResponseServerError("서버 오류 발생")
-    
-@router.get('/comments/{comment_id}', response=commentOut)
+        schedule = Schedule.objects.get(id=schedule_id)
+        user_id = 2
+        comment = Comment.objects.create(text=payload.text, schedule=schedule,user_id=user_id)
+        return {"success" : True}
+    except Schedule.DoesNotExist:
+        return 404, {"error": "Schedule not found"}
+
+# 해당 일정에 작성된 코멘트 불러오기 (미완)
+@router.get('/comments/{comment_id}', response=CommentOut)
 def get_Comment(request, comment_id: int):
     comment = get_object_or_404(Comment, id=comment_id)
     return comment
 
-# 전체 댓글 목록을 가져오는 엔드포인트
-@router.get("/comments/")
-def get_all_comments(request):
-    comments = Comment.objects.all()
-    comment_data = [{"id": comment.id, "text": comment.text, "timestamp": comment.timestamp} for comment in comments]
-    return comment_data
+# @router.get("/comments/")
+# def get_all_comments(request):
+#     comments = Comment.objects.all()
+#     comment_data = [{"id": comment.id, "text": comment.text, "timestamp": comment.timestamp} for comment in comments]
+#     return comment_data
 
 @router.put("/comments/{comment_id}")
-def update_Comment(request,comment_id: int, payload: commentIn):
+def update_Comment(request,comment_id: int, payload: CommentIn):
     comment = get_object_or_404(Comment, id=comment_id)
     comment.user_id = payload.user_id
     comment.timestamp = payload.timestamp
@@ -60,8 +66,53 @@ def update_Comment(request,comment_id: int, payload: commentIn):
     comment.save()
     return {"success" : True}
 
+# 코멘트 삭제
 @router.delete("/comments/{comment_id}")
 def delete_Comment(request,comment_id: int):
     comment = get_object_or_404(Comment, id=comment_id)
     comment.delete()
     return {"success" : True}
+
+# # 스케줄 생성
+# @router.post("/schedules/", response_model=ScheduleOut)
+# def create_schedule(schedule: ScheduleIn):
+#     db_schedule = Schedule(**schedule.dict())
+#     db_schedule.save()
+#     return db_schedule
+
+# # 모든 스케줄 조회
+# @router.get("/schedules/", response_model=List[ScheduleOut])
+# def get_all_schedules():
+#     schedules = Schedule.objects.all()
+#     return list(schedules)
+
+# # 특정 스케줄 조회
+# @router.get("/schedules/{schedule_id}/", response_model=ScheduleOut)
+# def get_schedule(schedule_id: int = Path(..., title="Schedule ID")):
+#     try:
+#         schedule = Schedule.objects.get(id=schedule_id)
+#         return schedule
+#     except Schedule.DoesNotExist:
+#         return {"Schedule not found"}
+
+# # 특정 스케줄 수정
+# @router.put("/schedules/{schedule_id}/", response_model=ScheduleOut)
+# def update_schedule( schedule: ScheduleIn,schedule_id: int = Path(..., title="Schedule ID")):
+#     try:
+#         db_schedule = Schedule.objects.get(id=schedule_id)
+#         for attr, value in schedule.dict().items():
+#             setattr(db_schedule, attr, value)
+#         db_schedule.save()
+#         return db_schedule
+#     except Schedule.DoesNotExist:
+#         return {"Schedule not found"}
+
+# # 특정 스케줄 삭제
+# @router.delete("/schedules/{schedule_id}/", response_model=ScheduleOut)
+# def delete_schedule(schedule_id: int = Path(..., title="Schedule ID")):
+#     try:
+#         schedule = Schedule.objects.get(id=schedule_id)
+#         schedule.delete()
+#         return schedule
+#     except Schedule.DoesNotExist:
+#         return {"Schedule not found"}
