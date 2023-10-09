27aedf7316d2ac53f20db49a04d4fd52328859b6
avocad06 <miggo2704@gmail.com>
2023-10-10 02:44:39 +0900

    feat: 로그인 권한 페이지에서 수시로 로그인 중인지 확인 기능 추가 modified: 권한이 필요한 페이지마다 쿼리 요청 -> 이미 존재하는 userInfo refetch 함수로 수정 modified: CurrentUserContextProvider 위치 수정

--- front/src/components/Layout/GeneralLayout.tsx
+++ front/src/components/Layout/GeneralLayout.tsx
@@ -4,6 +4,7 @@ import { NavbarContent } from "../../router";
 
 /* React-Query */
 import { useUserQuery } from "../../hooks/useUserQuery";
+import CurrentUserContextProvider from "../../context/CurrentUserContextProvider";
 
 export default function GeneralLayout() {
   const userQuery = useUserQuery();
@@ -19,8 +20,10 @@ export default function GeneralLayout() {
 
   return (
     <>
-      <Navbar NavbarContent={navbarLists()} />
-      <Outlet />
+      <CurrentUserContextProvider>
+        <Navbar NavbarContent={navbarLists()} />
+        <Outlet />
+      </CurrentUserContextProvider>
     </>
   );
 }
