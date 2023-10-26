a285baea404b05b4dd01ee0755cf1abc557de73d
an dong woo <akskwha@naver.com>
2023-10-26 15:48:54 +0900

    스케줄 카테고리분리(일정,출석), 스케줄 365일 생성기능추가,댓글 생성수정,스케줄 조회수정

--- back/schedule/api.py
+++ back/schedule/api.py
@@ -3,12 +3,13 @@ from potato.models import Comment, User, Schedule
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseServerError
 from django.contrib.auth.decorators import login_required
-from datetime import date
+from datetime import date,timedelta
 from pydantic import BaseModel
 from typing import List, Optional
 import logging
 from django.contrib.auth.decorators import user_passes_test#슈퍼유저만
 from django.http import HttpResponse
+from django.utils import timezone
 router = Router()
 logger = logging.getLogger(__name__)
 
@@ -31,12 +32,14 @@ class ScheduleIn(Schema):
     start_date: date
     schedule: str
     is_holiday: bool
-
+    category: str
+    
 class ScheduleOut(Schema):
     id: int
     start_date: date
     schedule: str
     is_holiday: bool
+    category: str
     # is_staff:bool#스태프 여부
 
 # 댓글 작성 (특정 스케줄에 맞춰)
@@ -49,12 +52,32 @@ def create_Comment(request, schedule_id:int, payload: CommentIn):
         schedule = Schedule.objects.get(id=schedule_id)
         comment = Comment.objects.create(text=payload.text, schedule=schedule,user=user)
         comment.save()
-        return {"id" : comment.id,
+        user_info=User.objects.get(id=user.id)
+        user_data = {#해당 유저 정보
+            "id": user_info.id,
+            "username": user_info.username,
+            "first_name": user_info.first_name,
+            "birth": user_info.birth,
+            "address": user_info.address,
+            "phone": user_info.phone,
+            "MBTI": user_info.MBTI,
+            "position": user_info.position,
+            "github": user_info.github,
+            "blog": user_info.blog,
+            " profile_image": user_info.profile_image
+        }
+        response_data = {
+            "comment": {
+                "id": comment.id,
                 "schedule_id": comment.schedule.id,
                 "user_id": user.id,
                 "timestamp": comment.timestamp,
-                "text": comment.text
-                }
+                "text": comment.text,
+            },
+            "user_info": user_data,
+        }
+        return response_data
+
     except Schedule.DoesNotExist:
         return {"error": "Schedule not found"}
 
@@ -120,18 +143,49 @@ def create_schedule(request,payload:ScheduleIn):
             end_date = payload.start_date,
             schedule = payload.schedule,
             is_holiday = payload.is_holiday,
+            category=payload.category
         )
         schedule.save()
         return {"id": schedule.id,
                 "start_date": schedule.start_date,
                 "schedule": schedule.schedule,
                 "is_holiday": schedule.is_holiday,
+                "category":schedule.category
                 }
     else:
         response = HttpResponse("권한이 없습니다.")
         response.status_code = 403
         return response
-      
+    
+#1년 스케줄 생성(카테고리 출석)
+@router.post("/create_yearly_schedules", tags=["스케줄"])
+@user_passes_test(lambda u: u.is_staff)
+@login_required
+def create_yearly_schedules(request):
+    # 시작일과 끝일 설정
+    today = timezone.now()
+    end_date = today + timedelta(days=365)  # 1년 후
+
+    # 스케줄 생성
+    while today <= end_date:
+
+        day_of_week = today.weekday()
+
+        # 주말제외
+        if day_of_week != 6 and day_of_week != 5:
+            schedule = Schedule.objects.create(
+                start_date=today,
+                end_date=today,
+                schedule="일일 스케줄",
+                is_holiday=False,
+                category='출석',  # 또는 '일정'으로 변경 가능
+            )
+            schedule.save()
+
+        today += timedelta(days=1)
+    
+    return {"message": "1년 동안의 스케줄이 생성되었습니다."}   
+
 # 특정 스케줄 조회
 @router.get("/schedules/{schedule_id}/", tags=["스케줄"], response=ScheduleOut)
 def get_schedule(request, schedule_id: int):
@@ -143,15 +197,16 @@ def get_schedule(request, schedule_id: int):
             end_date=schedule.start_date,
             schedule=schedule.schedule,
             is_holiday=schedule.is_holiday,
+            category=schedule.category
         )
     except Schedule.DoesNotExist:
         return 404, {"error": "Schedule not found"}
 
-# 내일까지 엔드데이트랑 스타트데이트
+# 스케줄조회
 @router.get("/schedules/", response=List[ScheduleOut], tags=["스케줄"])
-def get_schedules_in_date_range(request, from_date: date, to_date: date):
+def get_schedules_in_date_range(request, from_date: date, to_date: date,schedule_category:str):
     # start_date와 end_date 사이의 스케줄을 데이터베이스에서 조회
-    schedules = Schedule.objects.filter(start_date__gte=from_date, start_date__lte=to_date)
+    schedules = Schedule.objects.filter(start_date__gte=from_date, start_date__lte=to_date,category=schedule_category)
     
     # 조회된 스케줄을 ScheduleOut 형태로 변환하여 응답
     schedule_list = []
@@ -161,6 +216,7 @@ def get_schedules_in_date_range(request, from_date: date, to_date: date):
             "start_date": schedule.start_date,
             "schedule": schedule.schedule,
             "is_holiday": schedule.is_holiday,
+            "category":schedule.category
         }
         schedule_list.append(ScheduleOut(**schedule_data))
     
@@ -178,6 +234,7 @@ def update_schedule(request, schedule_id: int, payload: ScheduleIn):
             schedule.end_date = payload.start_date
             schedule.schedule = payload.schedule
             schedule.is_holiday = payload.is_holiday
+            schedule.category=payload.category
             schedule.save()
 
             return ScheduleOut(
@@ -186,6 +243,7 @@ def update_schedule(request, schedule_id: int, payload: ScheduleIn):
                 end_date=schedule.end_date,
                 schedule=schedule.schedule,
                 is_holiday=schedule.is_holiday,
+                category=payload.category,
                 is_staff=True
             )
         except Schedule.DoesNotExist:
